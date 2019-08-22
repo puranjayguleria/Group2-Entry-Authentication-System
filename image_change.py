1ffe6e94cf7d0fb0721158a5688352f2c6ec6570
Rhythm-Balooni <baloonirhythm@gmail.com>
2019-08-22 16:01:02 +0530

    The OpenCV module is used since it works better

--- image_change.py
+++ image_change.py
@@ -1,14 +1,13 @@
-from PIL import Image,ImageFilter
 import os
+import cv2
 
-size_96 = (96,96) #changes the size of image
-os.chdir("C:/Users/MUJ/Desktop")
-
-for f in os.listdir():
-    if f.endswith('.gif'):
-        i = Image.open(f)
-        fn, text = os.path.splitext(f)
-        i.thumbnail(size_96)
-        os.chdir("C:/Users/MUJ/Desktop")
-        i.convert('RGB').save('{}.jpg'.format(fn))
-        print("File extension changed and image saved")
+def image_changer():
+    #out = pil_im.resize((128,128))
+    os.chdir("C:/Users/MUJ/Desktop/TechSys/image_new")
+    
+    for f in os.listdir():
+        if f.endswith('.jpg'):
+            i = cv2.imread(f)
+            resized = cv2.resize(i, (96,96), interpolation = cv2.INTER_AREA)
+            cv2.imwrite(f, resized)
+            print("File extension changed and image saved")
